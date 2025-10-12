40386765df5a4ba62a4a34de954ebedab081a49c
Brian Egan <brianjoseph.egan@gmail.com>
2025-10-12 13:40:48 -0700

    Reading line by line from stream. (#3)

--- HttpFromTcp/HttpFromTcp/Program.cs
+++ HttpFromTcp/HttpFromTcp/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System.Text;
 
-var fileStream = File.OpenRead("messages.txt");
-var buffer = new byte[8];
-var read = await fileStream.ReadAsync(buffer, 0, buffer.Length);
+using var fileStream = File.OpenRead("messages.txt");
 
-while (read != 0)
+static async Task<string?> ReadStringFromStream(Stream stream, int length)
 {
-    Console.Out.WriteLine($"read: {Encoding.UTF8.GetString(buffer)}");
-    buffer = new byte[8];
-    read = await fileStream.ReadAsync(buffer, 0, buffer.Length);
+    var buffer = new byte[length];
+    var read = await stream.ReadAsync(buffer, 0, buffer.Length);
+    if (read > 0)
+    {
+        return Encoding.UTF8.GetString(buffer, 0, read).Replace("\r", string.Empty);
+    }
+    else
+    {
+        return null;
+    }
 }
+
+var stringBuilder = new StringBuilder();
+while (await ReadStringFromStream(fileStream, length: 8) is string streamText)
+{
+    var newlineIndex = streamText.IndexOf('\n', 0);
+    if (newlineIndex != -1)
+    {
+        stringBuilder.Append(streamText, 0, newlineIndex + 1);
+        Console.Out.Write($"read: {stringBuilder.ToString()}");
+        stringBuilder.Clear();
+        stringBuilder.Append(streamText.Substring(newlineIndex + 1));
+    }
+    else
+    {
+        stringBuilder.Append(streamText);
+    }
+}
+
+Console.Out.Write($"read: {stringBuilder.ToString()}");
